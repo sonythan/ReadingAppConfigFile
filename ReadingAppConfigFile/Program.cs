619203a7e8de298c3b1cc43131328abb559b9ffc
Sony Than <sonythan@gmail.com>
2020-07-19 23:07:04 +0700

    adding for Branch 1

--- ReadingAppConfigFile/Program.cs
+++ ReadingAppConfigFile/Program.cs
@@ -22,6 +22,7 @@ namespace ReadingAppConfigFile
             AddUpdateAppSettings("Setting1", "May 8, 2020");
             ReadAllSettings();*/
 
+            System.Console.WriteLine("Adding For Branch1")
             System.Console.ReadKey();
         }
 
